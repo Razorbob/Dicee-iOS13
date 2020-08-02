a8b5794bbf9c7c5ce8945206bb3e0a3c23adaa51
Vincent Dibon <vincent.dibon@gmail.com>
2020-08-02 16:48:35 +0200

    fork and upload my Code
    Forked from Appbrewery for the tutorial.
    
    Followed Tutorial and wrote the App.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    let diceImageArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setRandomDiceNumber()
+        
+    }
+    @IBAction func onRollButtonClicked(_ sender: UIButton) {
+        setRandomDiceNumber()
+    }
+    
+    func setRandomDiceNumber(){
+        
+        //Set Image randome from 0 to 5
+        diceImageView1.image = diceImageArray[Int.random(in: 0...5)]
+        
+        // Does the same just with the array function
+        diceImageView2.image = diceImageArray.randomElement()
     }
 
 
